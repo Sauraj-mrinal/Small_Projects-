27a980e92df8f60bf3bec29c1db99112d13020da
Sauraj-mrinal <msauraji@gmail.com>
2023-12-20 12:30:25 +0530

    Add

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,5 +1,6 @@
 console.log("hii ");
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomNumber1 = Math.floor( Math.random() * 6) + 1; //1-6
 
 var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
@@ -18,6 +19,7 @@ document.querySelectorAll("#img")[1].setAttribute("src", randomImageSource2);
 
 
 //If player 1 wins
+
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h3").innerHTML = "🚩 Play 1 Wins!";
 }
